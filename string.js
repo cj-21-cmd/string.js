94322baee23286b822525a77ec386bf17b3e1dc3
cjdelacruz <cjdelacruz0521@gmail.com>
2024-09-13 18:42:04 +0700

    Initial commit with string.js

---
+++ string.js
@@ -0,0 +1,23 @@
+//Concatenate
+let favActorFirstName = "Daniel";
+console.log("First name:", favActorFirstName);
+let favActorLastName = "Radcliffe";
+console.log("Last name:", favActorLastName);
+let fullname = favActorFirstName + ' ' + favActorLastName;
+console.log(fullname);
+
+//Uppercase
+let uppercase = 'Daniel Radcliffe';
+console.log("Full name:", uppercase);
+
+//Lowercase
+let lowercase = 'HALI';
+console.log("Pet name:", lowercase);
+
+//Append
+let message = "My Favorite Actor is DANIEL RADCLIFFE";
+message = message += 'he is my favorite character in Harry Potter';
+console.log(message);
+
+
+
